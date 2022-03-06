beaa44b4cf12834d9a9e8cc087826cbf254c6a6a
Gonzalo Busto Musi <gonzalo.busto@gmail.com>
2022-03-06 20:32:18 +0100

    Fixed an encoding error loading a JSON

--- utils/json_files_manipulation.py
+++ utils/json_files_manipulation.py
@@ -6,8 +6,6 @@ log = logging.getLogger(__name__)
 
 
 def save_data(relative_path, file, data):
-
-
     try:
         with open(f'{root_dir}/{relative_path.strip("/")}/{file}.json', 'w', encoding='utf-8') as f:
             log.info(f'Saving data on {root_dir}/{relative_path.strip("/")}/{file}.json')
@@ -18,7 +16,7 @@ def save_data(relative_path, file, data):
 
 def load_data(relative_path, file):
     try:
-        f = open(f'{root_dir}/{relative_path.strip("/")}/{file}.json')
+        f = open(f'{root_dir}/{relative_path.strip("/")}/{file}.json', encoding="utf8")
         log.info(f'Loading data from {root_dir}/{relative_path.strip("/")}/{file}.json')
         return json.load(f)
     except BaseException as error:
